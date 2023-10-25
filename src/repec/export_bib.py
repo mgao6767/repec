b78143d8c06b80e04ec811fde4e0be2c62f000e2
Adrian Gao <mingze.gao@sydney.edu.au>
2023-10-26 10:50:57 +1100

    Export JoF

--- src/repec/export_bib.py
+++ src/repec/export_bib.py
@@ -2,6 +2,7 @@ import sqlite3
 import json
 import zlib
 import nameparser
+from titlecase import titlecase
 
 # Load local packages
 from . import settings
@@ -35,6 +36,14 @@ def export(handle: str = "", export_bib_file=settings.export_bib):
             with open(export_bib_file, "w", encoding="utf-8") as f:
                 for res in results:
                     parse_redif_rfs(res[0], f)
+        # Journal of Finance
+        case "repec:bla:jfinan":
+            # if not check_urls_jof(results):
+            #     raise RuntimeWarning("Some papers' urls are missing")
+            # Some do not have file-url
+            with open(export_bib_file, "w", encoding="utf-8") as f:
+                for res in results:
+                    parse_redif_jof(res[0], f)
 
 
 def check_urls_jfe(results: list):
@@ -51,6 +60,10 @@ def check_urls_jfe(results: list):
 
 
 def check_urls_rfs(results: list):
+    return check_urls_jfe(results)
+
+
+def check_urls_jof(results: list):
     return check_urls_jfe(results)
 
 
@@ -74,7 +87,7 @@ def parse_redif_jfe(redif_data: bytes, f):
             case "year", year:
                 bib["year"] = year
             case "title", title:
-                bib["title"] = title
+                bib["title"] = titlecase(title)
             case k, v:
                 bib[k] = v
     authors = [name for name in sorted(authors)]
@@ -133,7 +146,7 @@ def parse_redif_rfs(redif_data: bytes, f):
             case "year", year:
                 bib["year"] = year
             case "title", title:
-                bib["title"] = title
+                bib["title"] = titlecase(title)
             case k, v:
                 bib[k] = v
     authors = [name for name in sorted(authors)]
@@ -149,3 +162,84 @@ def parse_redif_rfs(redif_data: bytes, f):
     print(f"@article{{{citekey},", file=f)
     print("\n".join(f"{k} = {{ {v} }}," for k, v in bib.items()), file=f)
     print("}", file=f)
+
+
+def parse_redif_jof(redif_data: bytes, f):
+    redif = zlib.decompress(redif_data)
+    redif = json.loads(redif)
+    bib = dict()
+    authors = []
+    for rec in redif:
+        match rec[0], "".join(rec[1:]):
+            case "template-type", _:
+                pass
+            # JoF uses all uppercase for author names
+            case "author-name", author:
+                name = nameparser.HumanName(author)
+                name.first = name.first.title()
+                name.middle = name.middle.title()
+                name.last = name.last.title()
+                name = f"{name.last}, {name.first} {name.middle}"
+                authors.append(name)
+            case "file-url", url:
+                # Not working
+                if url.startswith("http://links.jstor.org/sici"):
+                    url = ""
+                bib["url"] = url
+            case "abstract", abstract:
+                abstract = "".join(abstract.splitlines())
+                # non-breaking space to normal space
+                abstract = abstract.replace("\u00A0", " ")
+                # "Copyright XXXX by American Finance Association"
+                if (idx := abstract.rfind("Copyright ")) != -1 and abstract.find(
+                    " by American Finance Association"
+                ) != -1:
+                    abstract = abstract[:idx]
+                bib["abstract"] = abstract
+            case "year", year:
+                bib["year"] = year
+            case "title", title:
+                title = titlecase(title.lower())
+                title = title.replace("Nyse", "NYSE")
+                title = title.replace("Otc", "OTC")
+                title = title.replace("Capm", "CAPM")
+                title = title.replace("Ipo", "IPO")
+                title = title.replace("Seo", "SEO")
+                bib["title"] = title
+            # Drop the unnecessary
+            case "x-bibl", _:
+                pass
+            case k, v:
+                bib[k] = v
+    authors = [name for name in sorted(authors)]
+    bib["author"] = " AND ".join(authors)
+    # JFE missing journal fiel
+    bib["journal"] = "The Journal of Finance"
+
+    citekey = authors[0].strip().split(",")[0]
+    citekey += year
+    citekey += title.split(" ")[0]
+    citekey = citekey.lower()
+
+    print(f"@article{{{citekey},", file=f)
+    print("\n".join(f"{k} = {{ {v} }}," for k, v in bib.items()), file=f)
+    print("}", file=f)
+
+
+def format_last_name(name: str) -> str:
+    for pre in [
+        "de ",
+        "von ",
+        "van ",
+        "van der ",
+        "de la ",
+        "de los ",
+        "da ",
+        "dos ",
+    ]:
+        if name.last.lower().startswith(pre):
+            name.last = pre + name.last[len(pre) :]
+    if name.last.lower() == "deyoung":
+        name.last = "DeYoung"
+    if name.last.lower() == "mcdonald":
+        name.last = "McDonald"
